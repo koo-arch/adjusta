32a79c1e0ae2f903a8d4f1d06e852a861409b629
koo-arch <yukshw.ko.0623@gmail.com>
2025-04-25 15:56:54 +0000

    feat: Redirect to event list page if event is not found

--- src/features/events/detail/EventDetail.tsx
+++ src/features/events/detail/EventDetail.tsx
@@ -1,16 +1,27 @@
 'use client'
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useFetchEventDetail } from '@/hooks/event/useFetchEventDetail';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import DetailCard from './DetailCard';
 
 const EventDetail = () => {
     const params = useParams<{slug: string}>();
+    const router = useRouter();
 
     const { eventDetail, isLoading, error } = useFetchEventDetail(params.slug);
 
+    useEffect(() => {
+        if (!isLoading && (!eventDetail || error)) {
+            router.replace('/schedule/draft');
+        }
+    },[isLoading, eventDetail, error, router]);
+
     if (isLoading) {
         return <p>Loading...</p>;
+    }
+
+    if (error || !eventDetail) {
+        return null;
     }
 
     return (
